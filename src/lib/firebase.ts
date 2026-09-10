7aeadba373417c279ca794cf2f05ff5eca60287c
Iht-addmission <xoysharif@gmail.com>
2026-09-10 16:48:41 +0600

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,9 +3,8 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getFirestore, initializeFirestore } from 'firebase/firestore';
 import firebaseConfig from '../../firebase-applet-config.json';
-
+import { fetchAbout } from './path-to-this-file';
 const app = initializeApp(firebaseConfig);
-
 export const db = initializeFirestore(app, {
   experimentalForceLongPolling: true,
   ignoreUndefinedProperties: true,
